abaad7f5647135abdd895600831ec7dce8243519
Eliel-DM <elieldiasmatos@gmail.com>
2026-01-15 03:17:52 -0300

    Realizando exercicios até a aula de comparação de igualdade

---
+++ expressoesAndOperadores/autoIncrementoAndDecremento/autoIncrementoAndDecremento.php
@@ -0,0 +1,17 @@
+<?php
+
+$x = 0;
+$y = 0;
+
+$x++;
+$y = $y + 1;
+
+echo "valor de X: " . $x . "<br> Valor de Y: " . $y;
+
+$n = 10;
+$m = 10;
+
+$n--;
+$m = $m - 1;
+
+echo "<br> valor de N: " . $n . "<br> Valor de M: " . $m;
