889924a223051e98b40cc6dd52bbd6105871680d
Eliel Dias Matos <eliel.matos@soluti.com.br>
2026-02-03 16:29:13 -0300

    Iniciando Módulo Arrays e finalizando o exercício 45

---
+++ arrays/adicionandoFimArray/adicionandoFimArray.php
@@ -0,0 +1,15 @@
+<?php
+
+    $arr = [1,2,3];
+    $arr [] = 4;
+
+    print_r($arr);
+    echo "<br>";
+
+
+    
+    $arr2 = [1,2,3,2,3,89];
+    $arr2 [] = 67;
+
+    print_r($arr2);
+    echo "<br>";
---
+++ arrays/range/exercicio45.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+    Crie um array com a função range 10 a 45;
+    Imprima todos os números com a soma de 6;
+    Se passar de 30 a soma, imprima também que o número é muito alto;
+*/
+
+
+$arr = range(10,45);
+
+print_r($arr);
+
+for($x = 0; $x < count($arr); $x++){
+     if ($arr[$x]+6 > 30){
+        echo "O número é muito alto";
+    }
+    echo $arr[$x] . "<br>";
+   
+}
