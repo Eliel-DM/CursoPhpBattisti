5f790f48537e492e682f5bc9c2e49ba88c49af96
Eliel-DM <elieldiasmatos@gmail.com>
2026-02-10 23:39:59 -0300

    Finalizando as aulas teoricas do módulo

---
+++ arrays/unindoArrays/unindoArrays.php
@@ -0,0 +1,10 @@
+<?php
+
+$arr1 = [1, 2, 3];
+$arr2 = [10, 40, 90];
+$arr3 = [2.9, 44.5, 43, 3];
+$arr4 = ["Eliel", "teste"];
+$arrMerge = array_merge($arr1, $arr2, $arr3, $arr4);
+
+print_r($arrMerge);
+echo "<br>";
