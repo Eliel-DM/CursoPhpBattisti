28c3c7d38c46fd6c7d2c796173cb00a3a748a1e9
Eliel-DM <elieldiasmatos@gmail.com>
2026-02-11 21:41:01 -0300

    Iniciando o módulo de PHP, e finalizando o exercício 53

---
+++ POO/instanciandoObjeto/exercicio52.php
@@ -0,0 +1,13 @@
+<?php
+
+
+/*
+    Crie uma classe Car;
+    Instancie tres objetos com esta classe;
+*/
+
+class Car {}
+
+$newCar1 = new Car;
+$newCar2 = new Car;
+$newCar3 = new Car;
---
+++ POO/metodos/metodos.php
@@ -0,0 +1,14 @@
+<?php
+
+
+class Pessoa
+{
+    function falar()
+    {
+        echo "Olá, eu sou um objeto! <br>";
+    }
+}
+
+$eliel = new Pessoa;
+
+$eliel->falar();
